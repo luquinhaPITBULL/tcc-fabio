39af9065efce885f1a6fb8bac7abaee0cdddfef1
Lucas Rocha <lucasrocha@Lucass-MacBook-Air.local>
2025-10-24 22:17:43 -0300

    feat: Adiciona rotas completas - Landing como página inicial, Equipe, Resultados, ModalidadeDetalhes e PlanilhaDetalhes

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
 import Dashboard from "./pages/Dashboard";
@@ -7,12 +8,17 @@ import Exercicios from "./pages/Exercicios";
 import Sobre from "./pages/Sobre";
 import Contato from "./pages/Contato";
 import Termos from "./pages/Termos";
+import Equipe from "./pages/Equipe";
+import Resultados from "./pages/Resultados";
+import ModalidadeDetalhes from "./pages/ModalidadeDetalhes";
+import PlanilhaDetalhes from "./pages/PlanilhaDetalhes";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -20,6 +26,10 @@ export default function Router() {
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/contato" element={<Contato />} />
         <Route path="/termos" element={<Termos />} />
+        <Route path="/equipe" element={<Equipe />} />
+        <Route path="/resultados" element={<Resultados />} />
+        <Route path="/modalidade/:id" element={<ModalidadeDetalhes />} />
+        <Route path="/planilha/:id" element={<PlanilhaDetalhes />} />
       </Routes>
     </BrowserRouter>
   );
